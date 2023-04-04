384ef816f7d8f29fefdbe6041ea3e40c9e780051
leandro <92leandrof@gmail.com>
2023-04-04 11:48:48 -0300

    feat: added login validation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
 
 function Login() {
+  const [loginEmail, setloginEmail] = useState('');
+  const [loginPassword, setloginPassword] = useState('');
+
+  const regex = /\S+@\S+\.\S+/;
+  const MIN_PASSWORD_LENGTH = 6;
+  const navigate = useNavigate();
+
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const storedUser = localStorage.getItem('user');
+    const user = JSON.parse(storedUser);
+    if (user && user.email === loginEmail && user.password === loginPassword) {
+      localStorage.setItem('isLoggedIn', true)
+      navigate('/');
+    } else {
+      alert('Invalid email or password');
+    }
+  }
+
   return (
     <div>
       <h1>iCook</h1>
@@ -12,6 +32,7 @@ function Login() {
           name="email"
           id="email"
           placeholder="Email"
+          onChange={(e) => setloginEmail(e.target.value)}
         />
 
         <input
@@ -19,9 +40,16 @@ function Login() {
           name="password"
           id="password"
           placeholder="Password"
+          onChange={(e) => setloginPassword(e.target.value)}
         />
 
-        <button type="submit">Login</button>
+        <button
+        type="submit"
+        onClick={handleLogin}
+        disabled={!regex.test(loginEmail) || loginPassword.length < MIN_PASSWORD_LENGTH}
+        >
+          Login
+        </button>
         <h3>Don't have an account? <Link to="/registry">Registry</Link></h3>
       </form>
     </div>
