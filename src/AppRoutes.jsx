ef9c5d5f94df76031e6737fd49b74dcd7d17c634
leandro <92leandrof@gmail.com>
2023-04-04 20:18:30 -0300

    feat: add foods and drinks route

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Drinks from "./pages/Drinks";
+import Foods from "./pages/Foods";
 import Home from "./pages/Home";
 import Login from './pages/Login';
 import Registry from './pages/Registry';
@@ -10,6 +12,9 @@ function AppRoutes() {
       <Route exact path="/" element={<Home/>} />
       <Route exact path="/login" element={<Login/>} />
       <Route exact path="/registry" element={<Registry/>} />
+      <Route exact path="/foods" element={<Foods/>} />
+      <Route exact path="/drinks" element={<Drinks/>} />
+
     </Routes>
   );
 }
