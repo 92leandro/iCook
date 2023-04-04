9e7a4ef5a431f6ae0e894e86966e6a5c75515dc9
leandro <92leandrof@gmail.com>
2023-04-04 12:19:30 -0300

    feat: add fetch api

---
+++ src/api/foodApi.js
@@ -0,0 +1,9 @@
+export const fetchAllFoods = async () => {
+  try {
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+    const data = await response.json();
+    return data.meals;
+  } catch (error) {
+    console.log(error);
+  }
+};
