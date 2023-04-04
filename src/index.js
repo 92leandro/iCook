78793e93aca75e48b29ca33d1c47120046403729
leandro <92leandrof@gmail.com>
2023-04-04 20:19:04 -0300

    feat: add query provider

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+const queryClient = new QueryClient();
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </React.StrictMode>
   </BrowserRouter>
 );
