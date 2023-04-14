e9afeb25fcb994bec3685dfd6803740c74aaa0ff
leandro <92leandrof@gmail.com>
2023-04-14 18:28:24 -0300

    feat: adding conditional rending

--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useQueries } from 'react-query';
-import FoodCard from "../components/FoodCard";
+import FoodCard from '../components/FoodCard';
 
 function Foods() {
   const [selectedCategory, setSelectedCategory] = useState('');
@@ -8,40 +8,57 @@ function Foods() {
   const queries = [
     {
       queryKey: 'categories',
-      queryFn: () => fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list').then((res) => res.json())
+      queryFn: () =>
+        fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list').then((res) => res.json()),
     },
     {
-      queryKey: 'meals',
+      queryKey: ['meals', selectedCategory],
       queryFn: () =>
         fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${selectedCategory}`).then((res) =>
           res.json()
-        )
+        ).then(data => data.meals),
+      enabled: !!selectedCategory,
+    },
+    {
+      queryKey: 'allMeals',
+      queryFn: () =>
+        fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=').then((res) =>
+          res.json()
+        ).then(data => data.meals),
     }
   ];
 
   const results = useQueries(queries);
 
   const handleCategoryChange = (event) => {
-    setSelectedCategory(event.target.value || null);
+    setSelectedCategory(event.target.value);
   };
 
-  if (results.some((result) => result.isLoading)) return <div>Loading...</div>;
-
   const categories = results[0].data?.meals;
-  const meals = results[1].data?.meals;
+  const meals = results[1].data;
+  const allMeals = results[2].data;
+
+  useEffect(() => {
+    results[2].refetch();
+  }, [results]);
+
+  if (results.some((result) => result.isLoading)) return <div>Loading...</div>;
 
   return (
     <div>
       <h1>Categories</h1>
-
-      {categories && categories.map((category) => (
-        <button key={category.strCategory} value={category.strCategory} onClick={handleCategoryChange}>
+      {categories && categories.map((category,index) => (
+        <button
+          key={index}
+          value={category.strCategory}
+          onClick={handleCategoryChange}
+        >
           {category.strCategory}
         </button>
       ))}
 
-       <FoodCard cards={meals} />
-       {console.log(meals)}
+      {selectedCategory ? meals && <FoodCard allMeals={meals} /> : <FoodCard allMeals={allMeals} />}
+
     </div>
   );
 }
